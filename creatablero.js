db169f5d50798cc8012599b44d91047ed175f824
Rubén <rmartinezdominguez@danielcastelao.org>
2022-01-17 18:44:39 +0100

    Primeros pasos

---
+++ creatablero.js
@@ -0,0 +1,16 @@
+function creatablero() {
+    var tablero = [["-", "-", "-", "", "+"], ["-", "-", "-", "-"], ["-", "-", "-", "-", "-"]];
+    var salida = "";
+    for (var i = 0; i < tablero.length; i++) {
+        for (var j = 0; j < tablero[i].length; j++) {
+            salida = salida + tablero[i][j] + " "
+        } salida = salida + "\n";
+    }
+    console.log(salida);
+
+
+
+
+
+
+}
